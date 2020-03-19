ac70d6894bcf4d21d16824a51fed160cb2b6b257
uytera <uytera12@yandex.ru>
2020-03-19 11:05:57 +0500

    Add immersive

--- app/src/main/java/com/example/firstappv2/MainActivity.java
+++ app/src/main/java/com/example/firstappv2/MainActivity.java
@@ -16,10 +16,12 @@ public class MainActivity extends AppCompatActivity {
     Button deepButton;
     ConstraintLayout view;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hideSystemUI();
 
         view = (ConstraintLayout) findViewById(R.id.currentAct);
         topText = (TextView) findViewById(R.id.topText);
@@ -29,23 +31,43 @@ public class MainActivity extends AppCompatActivity {
         middleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                hideSystemUI();
                 topText.setText("aurea mediocritas");
                 topText.setTextColor(Color.parseColor("#EFB135"));
                 middleButton.setVisibility(View.INVISIBLE);
                 deepButton.setVisibility(View.VISIBLE);
-                view.setBackgroundColor(R.drawable.gradient_middle);
+                view.setBackgroundResource(R.drawable.gradient_middle);
             }
         });
 
         deepButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                hideSystemUI();
                 topText.setText("tenebris profundis");
                 topText.setTextColor(Color.parseColor("#000000"));
                 middleButton.setVisibility(View.VISIBLE);
                 deepButton.setVisibility(View.INVISIBLE);
-                view.setBackgroundColor(R.drawable.gradient_deep);
+                view.setBackgroundResource(R.drawable.gradient_deep);
             }
         });
+    }
+
+    private void hideSystemUI() {
+        View mDecorView = getWindow().getDecorView();
+        mDecorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+
+    private void showSystemUI() {
+        View mDecorView = getWindow().getDecorView();
+        mDecorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 }
