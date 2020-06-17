3c7dd43762e0db2f995ebf9350ca39946ed9c8b2
uytera <uytera12@yandex.ru>
2020-06-17 11:04:17 +0500

    Not working test

--- app/src/androidTest/java/com/example/firstappv2/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/firstappv2/ExampleInstrumentedTest.java
@@ -1,14 +1,26 @@
 package com.example.firstappv2;
 
 import android.content.Context;
+import android.content.Intent;
 
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
 
+import com.android21buttons.fragmenttestrule.FragmentTestRule;
+import com.example.firstappv2.fragments.main.MainFragment;
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.TestCase.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,11 +29,13 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityTestRule<NavMainActivity> activityActivityTestRule = new ActivityTestRule<>(NavMainActivity.class);
+//    @Rule
+//    public FragmentTestRule<?, MainFragment> fragmentTestRule = FragmentTestRule.create(MainFragment.class);
+
     @Test
     public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-
-        assertEquals("com.example.firstappv2", appContext.getPackageName());
+        onView(withText(R.string.app_header)).check(matches(isDisplayed()));
     }
 }
